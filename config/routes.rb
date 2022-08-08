067aa7a07c3fe85d76f305038b5f306ac9218e3f
Samruddhi Gaikwad <samrudigaikwad01@gmail.com>
2022-08-08 12:01:23 +0530

    My blog application has been saved sucessfully.

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  # get 'pages/home'
+  # root 'application#hello'
+  root 'pages#home'
+  get '/about', to: 'pages#about'
+  # root 'pages#about'
+end
+ # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
